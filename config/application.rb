52379b9643e1f7075a240ea8ff1a6b60add6a446
LucianNovo <lucian.novo@gmail.com>
2013-03-24 21:10:23 -0700

    configuration commit

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Ideally
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.assets.precompile += ['style.css']
+    config.assets.precompile += ['avgrund.css']
   end
 end
